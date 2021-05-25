daf4349a8eaf07f059aa107c11b7a3403968037f
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2021-05-25 17:08:55 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace s4lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //zaloguj login
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // Rejestracja haslo
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+           //rejestracja powtowrz haslo
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                MessageBox.Show("RODO Musi być zaznaczone");
+            }
+
+            if (textBox3.Text != textBox5.Text)
+            {
+                MessageBox.Show("Hasla nie pasuja do siebie");
+            }else if (String.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Pole haslo nie moze być puste!");
+            }else if (String.IsNullOrEmpty(textBox5.Text))
+            {
+                MessageBox.Show("Pole haslo nie moze być puste!");
+            }else if (String.IsNullOrEmpty(textBox4.Text))
+            {
+                MessageBox.Show("Pole login nie moze być puste!");
+            }
+            else
+            {
+                Uzytkownicy uzytkownicy = new Uzytkownicy(textBox2.Text, textBox1.Text);
+                Uzytkownicy.DodajDoListy(uzytkownicy);
+                MessageBox.Show("Zarejestrowano pomyslnie");
+            }
+
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            //rejestracja login
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //zaloguj haslo
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Uzytkownicy.ListaOsob.Exists(x=>x.Login==textBox1.Text && x.Haslo == textBox2.Text))            {
+                MessageBox.Show("Haslo lub login poprawny");
+            }
+            else
+            {
+                MessageBox.Show("zalogowano");
+            }
+           }
+    }
+}
---
+++ uzytkownicy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s4lab5
+{
+    class Uzytkownicy
+    {
+        public static List<Uzytkownicy> ListaOsob = new List<Uzytkownicy>();
+        public string Login { get; set; }
+        public string Haslo { get; set; }
+        
+        public Uzytkownicy(string login, String haslo)
+        {
+            this.Login = login;
+            this.Haslo = haslo;
+        }
+        public static void DodajDoListy(Uzytkownicy uzytkownicy)
+        {
+            ListaOsob.Add(uzytkownicy);
+        }
+        public static void Zaloguj(Uzytkownicy uzytkownicy)
+        {
+            ListaOsob.Equals(uzytkownicy);
+      
+        }
+    }
+}
